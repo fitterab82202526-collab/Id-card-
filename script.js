2a33345031caa03ce2f0408a2ba7080801a759e1
fitterab82202526-collab <fitterab82202526@gmail.com>
2026-08-13 21:57:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL="https://script.google.com/macros/s/AKfycbzZ_ltyfqQovQjNeF_C90rUbRRRc11rFVKBmUPtgaP5Si9_UA6cM0-O0IvamZ3eQKG8/exec";
+const APPS_SCRIPT_URL="https://script.google.com/macros/s/AKfycbw7gG2dYAUDrgT6E98QDp1vKYPdhZ9Wgr9jyR1OoThZQ6CxlTB_n0q1e_WVOTajtQ1_/exec";
 const students=window.ITI_STUDENTS||[];let selected=null,photoData=null;
 const $=id=>document.getElementById(id);
 const norm=v=>String(v??"").toLowerCase().replace(/[^a-z0-9]+/g,"");
